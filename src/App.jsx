7120b5b5ee7dcf64beb589fc8d32bc795341a763
WidadurRohman <ngajibandungan@gmail.com>
2025-03-15 16:01:20 -0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Logo from '/logo.jpg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="avatar">
+      <div className="w-10 rounded">
+        <img src='./public/Logo.jpg'></img>
+      </div>
+    </div>
+      <div className=''> 
+        portofolio
+      </div>
+      <div>
+        zhyeeen
+      </div>
+    </>
+  )
+}
+
+export default App
